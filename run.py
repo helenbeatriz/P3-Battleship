dc7e1ee532f8121a24b9c8ec038e650b4e69911d
Helen Cantu <helen_beatriz02@outlook.com>
2022-11-29 21:32:07 +0000

    Adding user input and responses that are given based on it

--- run.py
+++ run.py
@@ -49,5 +49,38 @@ def get_ship_location():
         column = input("Enter the column of the ship: ").upper()
     return int(row) - 1, letters_to_numbers[column]
 
+# variable to check if all the ships available are hit
+def count_hit_ships(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+    return count
+
+if __name__ == "__main__":
+    create_ships(HIDDEN_BOARD)
+    turns = 10
+    while turns > 0:
+        print('Guess a battleship location')
+        print_board(GUESS_BOARD)
+        row, column = get_ship_location()
+        if GUESS_BOARD[row][column] == "-":
+            print("You inserted that one already.")
+        elif HIDDEN_BOARD[row][column] == "X":
+            print("Hit")
+            GUESS_BOARD[row][column] = "X" 
+            turns -= 1  
+        else:
+            print("MISS!")
+            GUESS_BOARD[row][column] = "-"   
+            turns -= 1     
+        if count_hit_ships(GUESS_BOARD) == 5:
+            print("You win!")
+            break
+        print("You have " + str(turns) + " turns left")
+        if turns == 0:
+            print("You ran out of turns")
+ 
 
             # criar hj a commit  # variable to check if all the ships available are hit
