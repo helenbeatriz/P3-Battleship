a6b222d1521e30b766d362c203044308e51110f3
Helen Cantu <helen_beatriz02@outlook.com>
2022-11-29 21:30:20 +0000

    board of the game

--- run.py
+++ run.py
@@ -5,3 +5,27 @@ from random import randint
 Welcome player
 """
 name = input("Enter your name here:\n")
+
+#Board for holding ship locations
+HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+# Board for displaying hits and misses
+GUESS_BOARD = [[" "] * 8 for i in range(8)]
+
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
