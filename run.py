5eb292eb44b695357dcc096d897a75b22a3c4a18
Helen Cantu <helen_beatriz02@outlook.com>
2022-12-04 15:08:54 +0000

    Fixing errors on CI Python Linter

--- run.py
+++ run.py
@@ -14,12 +14,13 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('battleship_names')
- 
+
 '''
 Welcome message and rules
 '''
 
-def welcome(): 
+
+def welcome():
     os.system('clear')
     print("Welcome to Battleships!")
     print("""
@@ -33,13 +34,15 @@ def welcome():
       \_______________________________________________WWS_________/
   wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
 wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-   wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww 
+   wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
 """)
     print("Our board consists of 8 rows and 8 columns.")
     print("Your goal is to find where the ships are")
-    print("After every input you type a row and a column letter into the game, press enter to continue.")
+    print("After every input you type a row and")
+    print(" a column letter into the game,")
+    print(" press enter to continue.")
     print("Whenever you get it right an 'X' will be displayed")
-    print("Whenever you get it wrong an '-' will be displayed") 
+    print("Whenever you get it wrong an '-' will be displayed")
 
 
 '''
@@ -60,6 +63,7 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+
 letters_to_numbers = {
     'A': 0,
     'B': 1,
@@ -70,15 +74,18 @@ letters_to_numbers = {
     'G': 6,
     'H': 7
 }
-''' 
-computer create 5 ships
 '''
+computer creates 5 ships
+'''
+
+
 def create_ships(board):
     for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
+        ship_row, ship_column = randint(0, 7), randint(0, 7)
         while board[ship_row][ship_column] == "X":
             ship_row, ship_column = get_ship_location()
         board[ship_row][ship_column] = "X"
+
 
 def get_ship_location():
     row = input("Enter the row of the ship: ").upper()
@@ -91,9 +98,12 @@ def get_ship_location():
         column = input("Enter the column of the ship: ").upper()
     return int(row) - 1, letters_to_numbers[column]
 
+
 '''
 check if all ships are hit
 '''
+
+
 def count_hit_ships(board):
     count = 0
     for row in board:
@@ -101,6 +111,11 @@ def count_hit_ships(board):
             if column == "X":
                 count += 1
     return count
+
+
+'''
+function to play the game
+'''
 
 
 def game():
@@ -114,12 +129,12 @@ def game():
             print("You guessed that one already.")
         elif HIDDEN_BOARD[row][column] == "X":
             print("Hit")
-            GUESS_BOARD[row][column] = "X" 
-            turns -= 1  
+            GUESS_BOARD[row][column] = "X"
+            turns -= 1
         else:
             print("MISS!")
-            GUESS_BOARD[row][column] = "-"   
-            turns -= 1     
+            GUESS_BOARD[row][column] = "-"
+            turns -= 1
         if count_hit_ships(GUESS_BOARD) == 5:
             print("You won!")
             break
@@ -127,8 +142,9 @@ def game():
         if turns == 0:
             print("You lost :( !! ")
 
+
 '''
-Calling function for the game
+Calling functions for the game
 '''
 if __name__ == "__main__":
     welcome()
